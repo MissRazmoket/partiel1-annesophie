6a2b4e57e8b816b8a82c6e62dcb18446320117e7
Anne-Sophie LERIC <anne.sophie.leric@gmail.com>
2018-11-23 13:32:07 +0100

    commit initil partiel1-annesophie

---
+++ partiel1-annesophie/src/main/java/fr/formation/partiel1/Transfer.java
@@ -0,0 +1,13 @@
+package fr.formation.partiel1;
+
+import java.time.LocalDateTime;
+
+public class Transfer {
+
+    private double amount;
+
+    private LocalDateTime tranferAskedDate;
+
+    private LocalDateTime transfertExécution;
+
+}
---
+++ partiel1-annesophie/src/test/java/fr/formation/partiel1/TransfertTest.java
@@ -0,0 +1,16 @@
+package fr.formation.partiel1;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class TransfertTest {
+
+    void ShoulConstruct() {
+	Transfer transfer1 = new Transfer();
+	assertNotNull(transfer1);
+    }
+
+}
+
+//void shoulNotConstruct() {
+//    Transfer transfer1 = new Transfer(234.45,  )
+//}
